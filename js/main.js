aae015832d092939d56e6ba494eff1de367af968
seb510 <sebistian@ukr.net>
2021-01-10 09:10:05 +0200

    media adaptive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 $(function(){
+    /* function slider slic*/
     $('.slider').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [
+           {
+                breakpoint: 1175,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                }
+           },
+           {
+                breakpoint: 769,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                }
+            }
+
+        ]
     });
+    
+    /*function burger*/
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list-active')
+    })
 });
